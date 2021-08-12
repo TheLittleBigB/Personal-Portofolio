ea2fb71efa2a44ccc70541cbf5a48b737a4b0a51
Indra Nurwibisono <bigmasterindra@yahoo.com>
2021-08-12 15:25:14 +0700

    change contact info

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $('.nav-link').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() <= 20) {
+        $('.navbar').css('background', 'black');
+        $('.navbar-brand').css('color', 'white');
+    } else {
+        $('.navbar').css('background', 'transparent');
+        $('.navbar-brand').css('color', 'black');
+    }
+});
